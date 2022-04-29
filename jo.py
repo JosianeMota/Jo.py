b254dcdcc7353b9ee7fb233d01ba86c39710b159
JosianeMota <102743914+JosianeMota@users.noreply.github.com>
2022-04-29 13:18:10 -0300

    Add files via upload

---
+++ jo.py
@@ -0,0 +1,10 @@
+primeiroNome = input("Digite seu primeiro nome: ")
+salario= input("Reais:")
+centavos= input("centavos:")
+salarioformatado= salario + "," centavos:
+primeiraletra= primeiroNome [0:1].upper()
+restantenome= primeiroNome [1:].lower
+primeiroNomeformatado= primeiraletra + restantenome
+mensagem= "O salário de " + primeiroNomeformatado + "no mês de maio foi de:"
+
+
